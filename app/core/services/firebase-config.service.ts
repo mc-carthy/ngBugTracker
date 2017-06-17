cb10f0d8ce57c68dd0931d1f74225d977c42ac5b
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-06-17 13:50:30 +0100

    Configure access to firebase database

--- app/core/services/firebase-config.service.ts
+++ app/core/services/firebase-config.service.ts
@@ -1,19 +1,28 @@
 import { Injectable } from '@angular/core';
 
 import * as firebase from 'firebase';
+require('firebase/databse');
 
 import { FIREBASE_CONFIG } from './../constants/constants';
 
 @Injectable()
 export class FirebaseConfigService {
 
+    private database: firebase.database.Database;
+
     constructor() {
         this.configureApp();
+        this.configureDatabase();
     }
 
     configureApp()
     {
         firebase.initializeApp(FIREBASE_CONFIG);
+    }
+
+    configureDatabase()
+    {
+        this.database = firebase.database();
     }
 
 }
