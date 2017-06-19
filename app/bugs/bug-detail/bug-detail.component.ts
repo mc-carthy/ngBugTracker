bfa6baff4c2c2717902c9b4f2f9f6df274e087b6
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-06-19 13:08:29 +0100

    Final tweaks

--- app/bugs/bug-detail/bug-detail.component.ts
+++ app/bugs/bug-detail/bug-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 
 import { forbiddenStringValidator } from './../../shared/validation/forbidden-string.validator';
@@ -24,7 +24,7 @@ export class BugDetailComponent implements OnInit {
     private severities = SEVERITY;
     private statusArr: string[] = [];
     private severityArr: string[] = [];
-    @Input() currentBug= new Bug(null, null, this.statuses.Logged, this.severities.Severe, null, null, null, null, null);
+    private currentBug = new Bug(null, null, this.statuses.Logged, this.severities.Severe, null, null, null, null, null);
 
     constructor(
         private formBuilder: FormBuilder,
